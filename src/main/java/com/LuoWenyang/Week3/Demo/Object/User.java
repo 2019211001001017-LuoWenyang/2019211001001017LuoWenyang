49c781dbe3c2f050134f2fe6e139fcf6936fd71f
mac <927519994@qq.com>
2021-03-22 23:35:26 +0800

    HW-CE2 Commit

---
+++ src/main/java/com/LuoWenyang/Week3/Demo/Object/User.java
@@ -0,0 +1,49 @@
+package com.LuoWenyang.Week3.Demo.Object;
+
+public class User {
+    public String username;
+    public String password ;
+    public String email ;
+    public String gender ;
+    public String birthDate ;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+}
