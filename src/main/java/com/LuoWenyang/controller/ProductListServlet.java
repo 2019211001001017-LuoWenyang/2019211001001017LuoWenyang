9ae58e6cb6a755ef0c4f53b9cd9dc5bb8b757b3c
mac <927519994@qq.com>
2021-05-19 12:40:31 +0800

    Week11 Commit

--- src/main/java/com/LuoWenyang/controller/ProductListServlet.java
+++ src/main/java/com/LuoWenyang/controller/ProductListServlet.java
@@ -5,7 +5,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "ProductListServlet", value = "/admin/productListServlet")
+@WebServlet(name = "ProductListServlet", value = "/admin/productList")
 public class ProductListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
